0b188faec62a40cb157e33aac5540c8a97acf3c0
ems174 <ems174@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2008-11-16 18:36:03 +0000

    Adding the new Gloworm package.
    git-svn-id: https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm@5822 ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8

---
+++ trunk/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+setup(name='Products.Gloworm',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
