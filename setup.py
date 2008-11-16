89e2e142aee6bfcd79908e59d9b4c4fc1c057aec
ems174 <ems174@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2008-11-16 21:58:06 +0000

    Adding upload instructions to setup.py.
    git-svn-id: https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm/trunk@5828 ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+# To build locally: python setup.py egg_info -RDb "" bdist_egg 
+# To release: python setup.py egg_info -RD sdist bdist_egg register upload
+# To create a named release: python setup.py egg_info -RDb "a1" sdist bdist_egg register upload
+# To release a dev build: python setup.py egg_info -rD sdist bdist_egg register upload
+# See http://peak.telecommunity.com/DevCenter/setuptools#release-tagging-options for more information.
+
+
 from setuptools import setup, find_packages
 import os
 
@@ -6,8 +13,8 @@ version = '1.0b1'
 setup(name='Products.Gloworm',
       version=version,
       description="",
-      long_description=open("Products/Gloworm/HISTORY.txt").read() + "\n\n" +
-                       open("Products/Gloworm/README.txt").read(),
+      long_description=open("Products/Gloworm/README.txt").read() + "\n\n" +
+                       open("Products/Gloworm/HISTORY.txt").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
