9dbba1b5e155c552e56bf492334cf6893bbc9f1e
ems174 <ems174@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2008-11-16 21:35:44 +0000

    Tagging 1.0b1 for release.
    git-svn-id: https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm@5827 ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8

---
+++ tags/1.0b1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+setup(name='Products.Gloworm',
+      version=version,
+      description="",
+      long_description=open("Products/Gloworm/HISTORY.txt").read() + "\n\n" +
+                       open("Products/Gloworm/README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      author='WebLion Group, Penn State University',
+      author_email='support@weblion.psu.edu',
+      url='https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'plone.app.customerize>=1.1.2',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
