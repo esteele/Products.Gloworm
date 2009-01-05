88900498bcf7a34625414d21efee7de1c8bf1f86
ems174 <ems174@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-01-05 14:29:23 +0000

    Making an alias for the DevelopmentMode import. Seeing "DebugMode" in the code seems more intuitive to me.
    git-svn-id: https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm@6368 ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8

--- trunk/src/Products/Gloworm/__init__.py
+++ trunk/src/Products/Gloworm/__init__.py
@@ -1,7 +1,7 @@
 from zope.interface import providedBy
 import binascii
 from zope.component import getGlobalSiteManager
-from Globals import DevelopmentMode
+from Globals import DevelopmentMode as DebugMode
 from Products.Gloworm.browser.interfaces import IGlowormLayer
 
 import logging
@@ -76,6 +76,6 @@ def initialize(context):
     from plone.app.viewletmanager.manager import BaseOrderedViewletManager
     # Only install the monkey patch if we're running in debug mode since that's one of the prereqs for running GloWorm. 
     # It's an attempt to minimize the effects this monkeypatch has on the system until we can get some sort of adaptation working properly.
-    if DevelopmentMode:
+    if DebugMode:
         BaseOrderedViewletManager.render = render
     logger.info('BaseOrderedViewletManager.render monkey patch installed.')
