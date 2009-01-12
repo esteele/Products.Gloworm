d162f573b7d82297b573f6c05180d780fcb33023
ems174 <ems174@ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8>
2009-01-12 17:02:27 +0000

    Fixing paths.
    git-svn-id: https://weblion.psu.edu/svn/weblion/weblion/Products.Gloworm@6480 ea9a3244-bef9-0310-a2a5-f2a3d2dd1ec8

--- trunk/setup.py
+++ trunk/setup.py
@@ -8,13 +8,13 @@
 from setuptools import setup, find_packages
 import os
 
-version = open(os.path.join("src", "Products", "Gloworm", "version.txt")).read().strip()
+version = open(os.path.join("Products", "Gloworm", "version.txt")).read().strip()
 
 setup(name='Products.Gloworm',
       version=version,
       description="A Firebug-like inspection tool for Plone",
-      long_description=open("src/Products/Gloworm/README.txt").read() + "\n\n" +
-                       open("src/Products/Gloworm/HISTORY.txt").read(),
+      long_description=open("Products/Gloworm/README.txt").read() + "\n\n" +
+                       open("Products/Gloworm/HISTORY.txt").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
